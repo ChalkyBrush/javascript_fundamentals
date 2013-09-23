15dd0a3eea31a5521c6dd861db7e4b5a9a42c889
rraciop <ryanracioppo@gmail.com>
2013-09-23 18:03:30 -0400

    For loops and objects assignments completed

--- js_fundamentals/forloops.js
+++ js_fundamentals/forloops.js
@@ -20,10 +20,52 @@ for (var i=0;i<=20;i++)
 
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
 
+for (var i=0;i<=10;i++)
+{ 
+	mult = i*9
+	console.log(i +"*9 = " + mult)
+}
+
 // Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+
+for (var i=0;i<=10;i++)
+{
+	for (var j=0;j<=10;j++)
+	{	
+	mult = i*j
+	console.log(i +"*"+j +" = " + mult)
+	}
+}
 
 // EXERCISE: The Grade Assigner
 
 // Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
 
+function assignGrade(score)
+{
+	if (score<=49)
+	{
+		return "F"
+	}
+	else if (score<=59 && score >=50 )
+	{
+		return "D"
+	}
+	else if (score<=69 && score >=60 )
+	{
+		return "C"
+	}
+	else if (score<=79 && score >=70 )
+	{
+		return "B"
+	}
+	else
+	{
+		return "A"
+	}
+}
 
+for (var i=60;i<=100;i++)
+{
+	console.log("for " + i +", you got a " + assignGrade(i))
+}
