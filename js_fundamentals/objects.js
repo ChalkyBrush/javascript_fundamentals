8eb9680197fe6aed1905f2777837a5b19e6423ba
rraciop <ryanracioppo@gmail.com>
2013-09-23 17:36:16 -0400

    js objects 2nd exercise completed

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -35,6 +35,30 @@ console.log(recipeCard.ingredients[i-1])
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
+var myBooks = [
+  {name: "Human Action", 
+   author: "Ludwig von Mises",
+   alreadyRead: true},
+  {name: "Harry Potter", 
+   author: "JK Rowling",
+   alreadyRead: true},
+   {name: "Pragmatic Programmer", 
+   author: "Andy Hunt, Dave Thomas",
+   alreadyRead: false},
+];
+
+for (var i=0;i<myBooks.length;i++)
+{
+	if (myBooks[i].alreadyRead == true)
+	{
+	console.log("You already read " + myBooks[i].name + " by " + myBooks[i].author)
+	}
+	if (myBooks[i].alreadyRead == false)
+	{
+	console.log("You still need to read " + myBooks[i].name + " by " + myBooks[i].author)
+	}
+}
+
 // EXERCISE: The Movie Database
 
 // It's like IMDB, but much much smaller!
