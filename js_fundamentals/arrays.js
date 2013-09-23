06d27d7639189f81dd72ec0aca59f40d0abc9c9f
rraciop <ryanracioppo@gmail.com>
2013-09-23 16:44:09 -0400

    js array exercise completed

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,10 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
+var top_choice = ["Poutine", "Pizza", "Pad Thai", "Beef & Broccoli", "Sushi"]
 
+for (var i=1;i<=top_choice.length;i++)
+{ 
+console.log("My #" +i+" choice is " + top_choice[i-1] +".")
+}
 
